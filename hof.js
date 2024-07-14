01a92f0b464a52f5f7cd4e5831379f99849b823d
m0slah <muhammedslahqadir@gmail.com>
2024-07-14 16:03:21 +0300

    Code Reusability

--- hof.js
+++ hof.js
@@ -22,15 +22,25 @@
 // const greetBye = createGreeter("good bye");
 // greetBye("world");
 
-function performOperationOnArray(array, operation) {
-  return array.map(operation);
-}
+// function performOperationOnArray(array, operation) {
+//   return array.map(operation);
+// }
 
-function double(number) {
-  return number * 2;
+// function double(number) {
+//   return number * 2;
+// }
+
+// const numbers = [1, 2, 3, 4, 5];
+// const doubleNumber = performOperationOnArray(numbers, double);
+
+// console.log(doubleNumber);
+
+function filterArray(array, condition) {
+  return array.filter(condition);
 }
 
 const numbers = [1, 2, 3, 4, 5];
-const doubleNumber = performOperationOnArray(numbers, double);
 
-console.log(doubleNumber);
+const evenNumbers = filterArray(numbers, (num) => num % 2 === 0);
+
+console.log(evenNumbers);
