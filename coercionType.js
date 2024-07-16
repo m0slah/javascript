c579f4fe15a9a9a74b477a2fbc2dfe1adf3a103b
m0slah <muhammedslahqadir@gmail.com>
2024-07-16 10:56:48 +0300

    return error value

--- coercionType.js
+++ coercionType.js
@@ -8,7 +8,21 @@ function multiply(firstNumber, secondNumber) {
 }
 
 // const result = multiply(3, 5);
+// const result = multiply(3, "5");
+
+// console.log(result);
+
+//////////////////////////////////////////
+// The function can return an error value:
+
+function multiply(firstNumber, secondNumber) {
+  if (typeof firstNumber !== "number" || typeof secondNumber !== "number") {
+    return NaN;
+  }
+  return firstNumber * secondNumber;
+}
+
+// const result = multiply(2, 5);
 const result = multiply(3, "5");
 
 console.log(result);
-
